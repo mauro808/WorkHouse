39c6b8bfcb2e587cbd8032c960d163f3fe9b1f2c
vicky <vicky.ospina@gmail.com>
2021-02-25 23:10:24 -0500

    botones

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -8,26 +8,34 @@
 @endif
     <div class="col-lg-11" align="center">
     </br>
-        <h2 class="text-monospace">Registrar Nueva Categoria</h2>
+        <h2 class="text-monospace">Registrar Nueva Categoría</h2>
     </div>
     <div class="col-lg-11">
         
     </div>
 </div>  
 <div class="row">
-<div class="col-lg-4"></div>
-<div class="col-lg-6">
-<form action="{{ route('agregarCategoria') }}" method="POST" class="w-50 p-3 mr-3 text-center">
+<div class="col-lg-3"></div>
+<div class="col-lg-5">
+<form action="{{ route('agregarCategoria') }}" method="POST" class="w-45 p-3 mr-3 text-center">
 @csrf 
 <div class="form-group">
-    <label for="nombreCategoria">Nombre Categoria:</label>
-    <input type="text" class="form-control" id="nombreCategoria" name="nombreCategoria" placeholder="nombre de la categoria"/>
+    <label for="nombreCategoria">Nombre Categoría:</label>
+    <input type="text" class="form-control" id="nombreCategoria" name="nombreCategoria" placeholder="Ingrese el nombre"/>
     </br>
-    <label for="Descripcion">Descripcion Categoria:</label>
-    <textarea class="form-control" id="Descripcion" name="Descripcion" rows="5" placeholder="Descripcion categoria"></textarea>
+    <label for="Descripcion">Descripción Categoría:</label>
+    <textarea class="form-control" id="Descripcion" name="Descripcion" rows="5" placeholder="Descripción categoría"></textarea>
 </div>
-<button type="submit" class="btn btn-primary">Registrar</button>
-<a class="btn btn-warning" href="{{ url ('categorias') }}">Cancelar</a>
+<button type="submit" class="btn btn-primary" style="margin: 20px">
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+</svg> Registrar</button>
+<a class="btn btn-warning" href="{{ url ('categorias') }}" style="margin: 20px">
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg> Cancelar</a>
 </form>
 </div>
 <div class="col-lg-2"></div>
--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -7,6 +7,7 @@
     </div>
 @endif
     <div class="col-lg-11" align="center">
+    </br>
         <h2 class="text-monospace">Registrar Nuevo Cliente</h2>
     </div>
     <div class="col-lg-11">
@@ -14,9 +15,9 @@
     </div>
 </div>  
 <div class="row">
-<div class="col-lg-4"></div>
+<div class="col-lg-3"></div>
 <div class="col-lg-6">
-<form action="{{ route('agregarCliente') }}" method="POST" class="w-50 p-3 mr-3 text-center">
+<form action="{{ route('agregarCliente') }}" method="POST" class="w-60 p-3 mr-3 text-center">
 @csrf 
 
 @error('nombreUsuario')
@@ -25,32 +26,57 @@
     </div>
 @enderror
 <div class="form-group">
-    <label for="idUsuario">Id User create:</label>
-    <input type="text" class="form-control" id="idUsuario" name="idUsuario" placeholder="Usuario Creador"/>
-    </br>
+
+    <label for="idUsuario"></label>
+    <input type="hidden" class="form-control" id="idUsuario" name="idUsuario" placeholder="Usuario Creador"/>
+   <div class="row">
+     <div class="col-6">
     <label for="nombreCliente">Nombre:</label>
     <input type="text" class="form-control" id="nombreCliente" name="nombreCliente" placeholder="Digite el Nombre"/>
+     </div>
+    <div class="col-6">
     <label for="tipoIdentificacion">Tipo Documento:</label>
     <select name="tipoIdentificacion" class="form-control">
-        <option selected>Escoge...</option>
+        <option selected>Seleccione</option>
         <option>TI</option>
         <option>CC</option>
         <option>Pasaporte</option>
-        <option>Cedula Extranjeria</option>
+        <option>Cedula Extranjería</option>
         <option>Otro</option>
       </select>
-    </br>
+     </div>
+       </div>
+       <div class="row">
+     <div class="col-6">
     <label for="numeroIdentificacion">Identificacion:</label>
     <input type="text" class="form-control" id="numeroIdentificacion" name="numeroIdentificacion" placeholder="Digite el Documento"/>
+    </div>
+    <div class="col-6">
     <label for="telefonoFijo">Telefono fijo:</label>
     <input type="text" class="form-control" id="telefonoFijo'" name="telefonoFijo" placeholder="Digite el Telefono Fijo"/>
+     </div>
+       </div>
+        <div class="row">
+     <div class="col-6">
     <label for="celular">Celular:</label>
     <input type="text" class="form-control" id="celular'" name="celular" placeholder="Digite el Celular"/>
+       </div>
+    <div class="col-6">
     <label for="direccion">Direccion:</label>
     <input type="text" class="form-control" id="direccion'" name="direccion" placeholder="Digite la Direccion"/>
 </div>
-<button type="submit" class="btn btn-primary">Registrar</button>
- <a class="btn btn-warning" href="{{ url ('clientes') }}">Cancelar</a>
+</div>
+</br>
+<button type="submit" class="btn btn-primary" style="margin: 20px">
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+</svg> Registrar</button>
+ <a class="btn btn-warning" href="{{ url ('clientes') }}" style="margin: 20px">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg> Cancelar</a>
 </form>
 </div>
 <div class="col-lg-2"></div>
