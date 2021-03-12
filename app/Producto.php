bee8fb2d1d1da62c7c2497945174fbec042e75c6
mauro808 <mauricio808@outlook.es>
2021-03-12 12:40:18 -0500

    Avanze de controlador de ventas

--- app/Producto.php
+++ app/Producto.php
@@ -9,20 +9,7 @@ class Producto extends Model
 
     public $timestamps = false;
 
-<<<<<<< HEAD
     protected $table = 'productos';
 
     protected $fillable = ['idCategoria','nombreProducto','existencias','medida','precio','estado'];
-=======
-    protected $fillable = [
-        'id',
-        'idCategoria',
-        'nombreProducto',
-        'existencias',
-        'medida',
-        'porcentajeIva',
-        'precio',
-        'estado',
-    ];
->>>>>>> c942a46fcccc909796afd6e688e060782bfc9ea6
 }
