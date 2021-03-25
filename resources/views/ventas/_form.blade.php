6925876daeecea2992a20b543459ed8ff716a624
vicky <vicky.ospina@gmail.com>
2021-03-25 10:28:51 -0500

    vistaventa

--- resources/views/ventas/_form.blade.php
+++ resources/views/ventas/_form.blade.php
@@ -1,4 +1,7 @@
-<div class="col-12 mt-5">
+</br>
+</br>
+<div class="row">
+<div class="col-6">
 <label for="idCliente">Cliente:</label>
 <select name="idCliente" class="form-control" required >
         <option selected>Seleccione</option>
@@ -7,7 +10,7 @@
         @endforeach
 </select>
 </div>
-<div class="col-12 mt-5">
+<div class="col-6">
 <label for="idUsuario">Usuario:</label>
 <select name="idUsuario" class="form-control" required >
         <option selected>Seleccione</option>
@@ -16,8 +19,10 @@
         @endforeach
 </select>
 </div>
-<div class="row mt-5">
-<div class="col-6" >
+</div>
+</br>
+<div class="row">
+<div class="col-6">
 <label for="idProducto">Producto:</label>
 <select name="idProducto" id="idProducto" class="form-control" required>
         <option selected>Seleccione</option>
@@ -26,34 +31,38 @@
         @endforeach
 </select>
 </div>
-<div class="col-3 " >
+<div class="col-6">
     <label for="cantidad">Cantidad:</label>
     
     <input type="number" class="form-control" id="cantidad" name="cantidad" placeholder="Digite la cantidad"/>
 </div>
-<div class="col-3 " >
+</div>
+</br>
+<div class="row">
+<div class="col-12">
     <label for="valorProducto">Precio:</label>
     <input type="number" class="form-control" id="valorProducto" name="valorProducto"/>
     <input type="hidden" class="form-control" id="Estado" name="Estado" value="Activo"/>
 </div>
 </div>
+</br>
 <div class="form-group">
-<button type="button" id="agregar" class="btn btn-dark float-right mt-5 mb-5">
-<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
-  <path d="M8 15A7 1 1 7 0 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-</svg> Agregar Producto</button>
+<button type="button" id="agregar" class="btn btn-outline-dark btn-lg float-right">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+        <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+      </svg> Agregar Producto</button>
 </div>
 <div class="form-group" style="margin-top:150px">
 
-<h4 class="card-title">Detalle de venta</h4>
+    <h3 class="card-title"><strong>Detalle de Venta</strong></h3>
 <div class="table-responsive col-md-12">
     <table id="detallesVenta" class="table table-striped">
     <thead>
         <tr>
             <th>Eliminar</th>
             <th>Producto</th>
-            <th>Precio(COL$)</th>
+            <th>Precio</th>
             <th>Cantidad</th>
             <th>Subtotal</th>     
         </tr>
@@ -64,7 +73,7 @@
                 <p align="right">Total</p>
             </th>
             <th colspan="4">
-                <p align="right"><span id="total">Col:0$</span></p>
+                <p align="right"><span id="total">$/ </span></p>
             </th>
         </tr>
         <tr>
@@ -72,7 +81,7 @@
                 <p align="right">Total a pagar</p>
             </th>
             <th colspan="4">
-                <p align="right"><span id="total_pagar_html" name="total_pagar_html">Col:0$</span>
+                <p align="right"><span id="total_pagar_html" name="total_pagar_html">$/ </span>
                 <input type="number" name="precioTotal" id="precioTotal" name="precioTotal"></p>
             </th>
         </tr>
--- resources/views/ventas/create.blade.php
+++ resources/views/ventas/create.blade.php
@@ -18,13 +18,13 @@
 
 </div>
 <div class="card-footer text-muted col-12" align="center">
-<button type="submit" class="btn btn-dark" style="margin: 10px">
+<button type="submit" class="btn btn-dark btn-lg" style="margin: 10px">
 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 1 1 7 0 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
 </svg> Registrar</button>
 {!! Form::close() !!}
-<a class="btn btn-light" href="{{ url ('ventas') }}" style="margin: 20px">
+<a class="btn btn-light btn-lg" href="{{ url ('ventas') }}" style="margin: 20px">
  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -55,7 +55,7 @@ function agregar(){
     if(idProducto != "" && cantidad != "" && cantidad > 0) {
         subtotal[cont] = cantidad * precio;
         total = total + subtotal[cont];
-        var fila = "<tr class='selected' id='fila" + cont + "'><td><button type='button' class='btn btn-danger btn-sm' onclick='eliminar("+ cont + ")';><i class='fa fa times'></i></button></td><td><input type='hidden' name='idProducto[]' value='" + idProducto + "'>" + nombreProducto + "</td><td><input type='hidden' name='precio[]' id='precio[]' value='" + precio + "'><input class='form-control' type='number' id='precio[]' value='" + precio + "'disabled></td><td><input type='hidden' name='cantidad[]' id='cantidad[]' value='" + cantidad + "'><input class='form-control' type='number' value='" + cantidad + "'disabled></td><td align='right'>/col" + subtotal[cont] + "<td></tr>";
+        var fila = "<tr class='selected' id='fila" + cont + "'><td><button type='button' class='btn btn-danger btn-sm' onclick='eliminar("+ cont + ")';><i class='bi bi-trash'><strong></strong></i></button></td><td><input type='hidden' name='idProducto[]' value='" + idProducto + "'>" + nombreProducto + "</td><td><input type='hidden' name='precio[]' id='precio[]' value='" + precio + "'><input class='form-control' type='number' id='precio[]' value='" + precio + "'disabled></td><td><input type='hidden' name='cantidad[]' id='cantidad[]' value='" + cantidad + "'><input class='form-control' type='number' value='" + cantidad + "'disabled></td><td align='right'>$/ " + subtotal[cont] + "<td></tr>";
         cont++;
         console.log(cantidad, precio, subtotal[cont], total);
         limpiar();
@@ -64,9 +64,10 @@ function agregar(){
         $('#detallesVenta').append(fila);
     }else {
         Swal.fire({
+            type: 'error',
             icon: 'error',
-            title: 'Oops...',
-            text: 'Faltan Campos por diligencias.',
+            text: 'Rellene todos los campos del detalle de la venta',
+            confirmButtonColor: '#1C2833',
           })
     }
 }
@@ -77,9 +78,9 @@ function limpiar(){
 }
 
 function totales(){
-    $('#total').html("COL " + total.toFixed(2));
+    $('#total').html("$/ " + total.toFixed(2));
     precioTotal=total;
-    $("#total_pagar_html").html("COL " + precioTotal.toFixed(2));
+    $("#total_pagar_html").html("$/ " + precioTotal.toFixed(2));
     $("#precioTotal").val(precioTotal.toFixed(2));
 }
 
@@ -94,8 +95,8 @@ function evaluar(){
 function eliminar(index){
     total = total - subtotal[index];
     total_pagar_html = total;
-    $("#total").html("$" + total);
-    $("#total_pagar_html").html("$" + total_pagar_html);
+    $("#total").html("$/ " + total);
+    $("#total_pagar_html").html("$/ " + total_pagar_html);
     $("#total_pagar").val(total_pagar_html.toFixed(2));
     $("#precioTotal").val(total_pagar_html.toFixed(2));
     $("#fila" + index).remove();
